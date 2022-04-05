e829c6f3604aebc51f8523c0cde669be5f1f4cde
Pablo Alvarez Baeza <pablo.alvarez.baeza@gmail.com>
2022-04-05 15:06:33 +0200

    Updates

---
+++ 6.data_day_OWID.R
@@ -0,0 +1,128 @@
+library(pacman)
+p_load(tidyverse, janitor, showtext, ggtext, ggraph, igraph, camcorder, pathwork)
+
+gg_record(dir = "temp", device = "png", width = 16.5, height = 11.7, units = "in", dpi = 320)
+
+# Fonts
+font_add_google("Lato")
+font_add(family = "Lato black",
+         regular = "Lato-Black.ttf")
+
+# Data
+ life_expectancy <- read_csv("life-expectancy-of-women-vs-life-expectancy-of-women.csv") |> 
+   clean_names() |> 
+   select(country = entity,
+          year,
+          code,
+          birth_female = contains("birth_females"),
+          birth_male = contains("birth_males")) |> 
+   filter(year == 2020,
+          !is.na(code)) |> 
+   na.omit() |>
+   pivot_longer(cols = c(birth_male, birth_female), names_to = "gender") |> 
+   mutate(value = round(value, 0))
+ 
+ # Largest gender difference = Lithuania
+ life_expectancy |> 
+   group_by(country) |> 
+   mutate(diff =  value - lag(value)) |>
+   ungroup() |> 
+   slice_max(diff, n = 1) 
+ 
+
+# Male --------------------------------------------------------------------
+ life_expectancy_male <- life_expectancy |> 
+   filter(gender == "birth_male")  
+ 
+ life_expectancy_male |> 
+  summarize(n_countries = n_distinct(country),
+            max_life_expecetancy = max(value))
+
+life_expectancy_male |> 
+  slice_max(value, n = 1) 
+
+links_male <- data.frame(source = 0,
+                         value = c(1:88))
+
+links <- links_male |> 
+  left_join(life_expectancy_male, by = "value") |> 
+  mutate(country = if_else(is.na(country), "", country),
+         group = factor(case_when(country == "" ~ 1,
+                                  value == 70 ~ 3,
+                                  TRUE ~ 2))) |> 
+  select(source, destiny = value, group) |> 
+  group_by(destiny) |> 
+  mutate(width = factor(n()))
+
+graph <- graph_from_data_frame(links)
+
+# Male plot
+ggraph(graph, layout="linear") + 
+  geom_edge_arc(aes(edge_colour = group, edge_width = width, edge_alpha = group)) +
+  geom_node_text(aes(label=name), repel = FALSE, size = 3, color = "white", nudge_y = -1) +
+  scale_edge_color_manual(values = c("black","grey75", "#39FF14")) +
+  scale_edge_alpha_manual(values = c(0, .15, 1)) +
+  labs(title = "Life Expectancy by Sex",
+       subtitle = "In every country the life expectancy of <span style='color:#BB29BB'>women</span> is higher than the life expectancy of <span style='color:#39FF14'>men</span>") +
+  theme_void(base_family = "Lato") +
+  theme(plot.margin = margin(t = 40, r = 40, b = 0, l = 40),
+        panel.background = element_rect(fill = "black", color = "black"),
+        plot.background = element_rect(fill = "black", color = "black"),
+        plot.title = element_text(size = 48,
+                                  family = "Lato black",
+                                  color = "white"),
+        plot.subtitle = element_markdown(size = 24,
+                                  family = "Lato",
+                                  color = "white",
+                                  margin = margin(t = 10, b = 100)),
+        legend.position = "none")
+
+ggsave("30chartchallenge_6_male_2022.png", width = 16.5, height = 11.7, units = "in", dpi = 320)
+
+
+# Female ------------------------------------------------------------------
+life_expectancy_female <- life_expectancy |> 
+  filter(gender == "birth_female")  
+
+life_expectancy_female |> 
+  summarize(n_countries = n_distinct(country),
+            max_life_expecetancy = max(value))
+
+life_expectancy_female |> 
+  slice_max(value, n = 1) 
+
+links_female <- data.frame(source = 0,
+                         value = c(1:88))
+
+links <- links_female |> 
+  left_join(life_expectancy_female, by = "value") |> 
+  mutate(country = if_else(is.na(country), "", country),
+         group = factor(case_when(country == "" ~ 1,
+                                  value == 82 ~ 3,
+                                  TRUE ~ 2))) |> 
+  select(source, destiny = value, group) |> 
+  group_by(destiny) |> 
+  mutate(width = factor(n()))
+
+graph <- graph_from_data_frame(links)
+
+# Female plot
+ggraph(graph, layout="linear") + 
+  geom_edge_arc(aes(edge_color = group, edge_width = width, edge_alpha = group), strength = -1) +
+  geom_node_text(aes(label=name), repel = FALSE, size = 3, color = "white", nudge_y = .5) +
+  scale_edge_color_manual(values = c("black", "grey50", "#BB29BB")) +
+  scale_edge_alpha_manual(values = c(0, .15, 1)) +
+  labs(caption = "Visualization by Pablo Alvarez | Data from OurWorldInData: Life Expectancy at birth for the year 2020") +
+  theme_void(base_family = "Lato") +
+  theme(plot.margin = margin(t = 0, r = 40, b = 0, l = 40),
+        panel.background = element_rect(fill = "black", color = "black"),
+        plot.background = element_rect(fill = "black", color = "black"),
+        plot.caption = element_text(color = "white",
+                                    size = 12,
+                                    margin = margin(t = 60, b = 40),
+                                    hjust = .5),
+        legend.position = "none")
+
+ggsave("30chartchallenge_6_female_2022.png", width = 16.5, height = 11.7, units = "in", dpi = 320)
+
+# Exporting both plots to edit the last details in Figma
